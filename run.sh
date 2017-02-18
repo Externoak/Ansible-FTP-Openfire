4d2fff67179a84f1f7fda4dab01b616080d93504
Externoak <dariomr1234@gmail.com>
2017-02-18 23:19:51 +0100

    Working vsftpd+ mysql

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ vagrant up --no-provision
 echo "Provisionar"
 vagrant provision
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory tasks/main.yml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory tasks/mysql.yml
 echo "Finalizado"
