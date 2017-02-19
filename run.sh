6c489680ef2bcc4fc5a56fb18f5c759d490395e3
alfredobs97 <alfredobautista1@gmail.com>
2017-02-19 21:44:40 +0100

    Openldap

--- run.sh
+++ run.sh
@@ -5,4 +5,5 @@ echo "Provisionar"
 vagrant provision
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory tasks/main.yml
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory tasks/mysql.yml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory tasks/openldap.yml
 echo "Finalizado"
